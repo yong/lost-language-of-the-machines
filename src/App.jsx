adf49b977680174b42fe02290a9ef1b7dfa99875
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-14 14:53:17 -0400

    add Chapter 1 text

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
 import Cover from './Cover'
-
-const TextBox = () => {
-  return (
-      <div className='text-box'>
-          <h3>First Class Ticket</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores doloribus accusantium, quam odio
-          repudiandae mollitia eos sapiente quo labore magnam, dignissimos praesentium blanditiis ratione error
-          expedita, incidunt rerum corrupti modi enim commodi maxime veniam! Eius repellendus voluptates ducimus
-          perspiciatis officiis adipisci quibusdam amet, officia omnis quas id minus ipsam, aliquam qui voluptas quo
-          veritatis dolores vero magnam inventore sed numquam dignissimos a nisi. Tempore dolorem provident illo natus
-          velit temporibus fugiat odio aperiam a sunt et harum autem sequi officiis quos distinctio quis nihil, modi
-          eveniet commodi animi non accusantium! Nesciunt ut voluptatibus cumque et doloribus nihil impedit odio sed.
-      </p>
-      </div>
-  )
-}
+import Chapter1 from './Chapter1'
 
 function App() {
   return (
-    <div className={'w-full h-full'}>
+    <div>
       <Cover />
-      <TextBox />
+      <Chapter1 />
     </div>
   );
 }
---
+++ src/Chapter1.jsx
@@ -0,0 +1,59 @@
+const Chapter1 = () => {
+    return (
+        <div className='text-box'>
+            <h3>Chapter 1 "One and Zero"</h3>
+<p>Flamey dashed into the History Hall, his metal feet rattling on the carbon fiber floor. He quickly glanced at the displays on his time tracker, wondering if he was late for his first day of Archaeology: Programmatic and Computational Studies of the Past class. He felt a tug on his arm and turned to see Starlaxverse, his best human friend.</p>
+
+<p>“Oh, Flamey, I’m so relieved you’re here!” Starlaxverse exclaimed, dragging him along. “Professor Evergreen is known to be a person with ATTITUDE.”</p>
+
+<p>Flamey groaned. He wouldn’t have signed up for this class if he had a choice. It was known to be dull and perplexing. Why did he have to learn about the old languages from the Controlled Age that ancient computers used to speak? The textbook listed a lot of strange names like Binary, Assembly, C, Python, Javascript blah blah. They went extinct 500 years ago!</p>
+
+<p>They entered the room through a glass door, where Prof. Evergreen was sitting behind his desk with lots of empty chairs. As expected, they were the only two students today.</p>
+
+<p>“Well,” he muttered. “I guess I have to adapt, otherwise this class will not be offered next year. As history has taught us, modern kids have no patience for long lectures. For today’s topic –Binary, how about I start with a poem?”</p>
+
+<img/>
+
+<p>Flamey and Starlaxverse looked at each other, not sure what to say. With the eager look of their professor waiting for feedback on his “new” style of teaching, Flamey slowly raised his hand and opened up: “Err… I want to know why Binary was invented? Why didn't ancient computers use NORMAL math, like 1,2,3,4,5,6,7,8,9,10…?”</p>
+
+<p>“Convenience, young droid.” Prof. Evergreen elaborated, “Just like the fact that humans having ten fingers contributed to the popularity of the 10 based number system. Ancient computers used electric signals to communicate. So 1 represents On, 0 represents Off.” Professor gestured with his hand and a hologram of a cat flipping switch showed up. “By flipping the switch non stop, the cat generated the sequence of Ones and Zeros, which can be used to represent anything, including numbers”.</p>
+
+
+<img/>
+
+<p>“Wow.” Starlaxverse was instantly convinced. Cat pictures are ageless for little girls like her.</p>
+<p>“So here is the first rule of a number system”, Prof. Evergreen projected a slide in the air.</p>
+
+<img/>
+
+
+<p>“What if a number system’s base is larger than ten? Don’t we run out of numbers to use?” Flamey quickly found a way to argue - how can you blame him? As his brain cpu is set to teenage mode, argument is his specialty.</p>
+
+<p>“You can invent more symbols beyond 0 to 9, or just reuse existing symbols, like letters.” Prof. Evergreen replied as he had heard of this question millions of times. “For example, the 16-based number system (Hex), which is also very popular, uses: A for ten, B for eleven, C for twelve, D for thirteen, E for fourteen and F for fifteen.”</p>
+
+<p>“Oh…” Flamey was a little disappointed that his question was answered so easily.</p>
+
+<p>“Once we run out of single symbols – whatever the number is - we are going to combine them to build more complicated numbers. “ Prof. Evergreen continued, “Remember Place Value that you learned at Elementary school? It is the value of each digit in a number based on its position. Here is the second rule of a number system,” Prof. Evergreen casted another slide in the air:</p>
+
+<img/>
+
+<p>“Wow!” said Starlaxverse, not sure she was complimenting the rule or the cats on the slides wearing place value shirts.</p>
+
+<p>“Oh, it starts to make sense now.” As immune to any cuteness as he can be, Flamey found something: “So you can convert a binary number into decimal by adding up those parts.”</p>
+
+<p>“Good find.” Nodded Prof. Evergreen, ”But we ran out of time. As science has shown, your attention must be depleted now.” Prof. Evergreen walked out of the room in a rush without even acknowledging his two students.</p>
+
+<p>“Hmm…” Starlaxverse looked at Flamey with a puzzled look, “He is definitely a man with character, but not the way I expected.”</p>
+
+<p>“Yep.” Answered Flamey, “Don’t think he overused cats to make a point?”</p>
+
+<p>“Nope”, Said Starlaxverse, “There is no such thing as too many cats.”</p>
+
+
+
+        </div>
+    )
+  }
+
+
+export default Chapter1;
