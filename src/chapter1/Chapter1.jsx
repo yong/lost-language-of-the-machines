0a8ccd22314fe6cebc8e59232f6959220bfe0e61
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-15 18:54:59 -0400

    add Rule1

--- src/chapter1/Chapter1.jsx
+++ src/chapter1/Chapter1.jsx
@@ -1,15 +1,16 @@
 //Chapter1.jsx
 import Poem from './Poem';
+import Rule1 from './Rule1';
 
 const Chapter1 = () => {
     return (
         <article className="text-wrap p-6 md:p-12 lg:p-24">
             <h1 className="text-center text-2xl font-bold mb-4">Chapter 1 "One and Zero"</h1>
-<p className="mb-3 first-letter:text-7xl first-letter:font-bold first-letter:me-3 first-letter:float-start">Flamey dashed into the History Hall, his metal feet rattling on the carbon fiber floor. He quickly glanced at the displays on his time tracker, wondering if he was late for his first day of Archaeology: Programmatic and Computational Studies of the Past class. He felt a tug on his arm and turned to see Starlaxverse, his best human friend.</p>
+<p className="mb-3 first-letter:text-7xl first-letter:font-bold first-letter:me-3 first-letter:float-start">Flamey dashed into the History Hall, his metal feet rattling on the carbon-titanium floor. He quickly glanced at the displays on his time tracker, wondering if he was late for his first day of Archaeology: Programmatic and Computational Studies of the Past class. He felt a tug on his arm and turned to see Starlaxverse, his best human friend.</p>
 
 <p className="mb-3">“Oh, Flamey, I’m so relieved you’re here!” Starlaxverse exclaimed, dragging him along. “Professor Evergreen is known to be a person with ATTITUDE.”</p>
 
-<p className="mb-3">Flamey groaned. He wouldn’t have signed up for this class if he had a choice. It was known to be dull and perplexing. Why did he have to learn about the old languages from the Controlled Age that ancient computers used to speak? The textbook listed a lot of strange names like Binary, Assembly, C, Python, Javascript blah blah. They went extinct 500 years ago!</p>
+<p className="mb-3">Flamey groaned. He wouldn’t have signed up for this class if he had a choice. It was known to be dull and perplexing. Why did he have to learn about the old languages from the Controlled Silicon Age that ancient computers used to speak? The textbook listed a lot of strange names like Binary, Assembly, C, Python, Javascript blah blah. They went extinct 500 years ago!</p>
 
 <p className="mb-3">They entered the room through a glass door, where Prof. Evergreen was sitting behind his desk with lots of empty chairs. As expected, they were the only two students today.</p>
 
@@ -27,7 +28,7 @@ const Chapter1 = () => {
 <p className="mb-3">“Wow.” Starlaxverse was instantly convinced. Cat pictures are ageless for little girls like her.</p>
 <p className="mb-3">“So here is the first rule of a number system”, Prof. Evergreen projected a slide in the air.</p>
 
-<img/>
+<Rule1/>
 
 
 <p className="mb-3">“What if a number system’s base is larger than ten? Don’t we run out of numbers to use?” Flamey quickly found a way to argue - how can you blame him? As his brain cpu is set to teenage mode, argument is his specialty.</p>
