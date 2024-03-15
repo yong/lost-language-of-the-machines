1d7a03e2efce11d1d1f02f497551432bce209594
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-15 15:50:15 -0400

    better title and cover

--- src/Cover.jsx
+++ src/Cover.jsx
@@ -19,7 +19,7 @@ const Cover = () => {
           children: (
             <div className="absolute inset-0 flex items-center justify-center">
               <Snowfall color="white" snowflakeCount={200} images={images} />
-              <h1 className="backdrop-blur-sm text-center text-3xl font-bold text-white z-50">
+              <h1 className="backdrop-blur-sm text-center font-bold text-white z-50 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
                 Lost Language of the Machines
               </h1>
             </div>
