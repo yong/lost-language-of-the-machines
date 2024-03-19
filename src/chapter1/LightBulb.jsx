4e9bbeef700a05304e585c2332066587fac534b6
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-18 23:21:37 -0400

    add binary sequence when light is on and off

--- src/chapter1/LightBulb.jsx
+++ src/chapter1/LightBulb.jsx
@@ -1,15 +1,36 @@
 // LightBulb.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const LightBulb = () => {
   const [isOn, setIsOn] = useState(false);
+  const [sequence, setSequence] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update the sequence based on light bulb state
+      setSequence(prevSequence => {
+        const newBit = isOn ? '1' : '0';
+        const truncatedSequence = (prevSequence + newBit).slice(-20); // Keep the last 20 characters
+        return truncatedSequence;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isOn]);
 
   const toggleBulb = () => {
+    // Immediately add "1" or "0" to the sequence
+    const newBit = isOn ? '0' : '1';
+    setSequence(prevSequence => (prevSequence + newBit));
+
+    // Toggle the light bulb state
     setIsOn(!isOn);
   };
 
   return (
     <div className="flex flex-col md:flex-row h-screen">
+      
+
       {/* Left div */}
       <div id="left" className="md:w-1/2">
         <img src="/chapter1/cat.png" alt="Cat" />
@@ -17,6 +38,10 @@ const LightBulb = () => {
 
       {/* Right div */}
       <div id="right" className="md:w-1/2 flex flex-col items-center justify-center">
+        {/* Display sequence */}
+      <div className="md:w-1/2 p-4 overflow-hidden">
+        <p className="text-lg font-bold text-center">{sequence}</p>
+      </div>
         <img
           src={isOn ? '/chapter1/light-on.svg' : '/chapter1/light-off.svg'}
           alt="Light Bulb"
