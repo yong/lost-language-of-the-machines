521bb0f4e9ac348866b1d7d47ce93f5e682880d4
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-28 21:55:56 -0400

    more catchy button

--- src/chapter1/Chapter1.jsx
+++ src/chapter1/Chapter1.jsx
@@ -2,8 +2,8 @@
 import Poem from './Poem';
 import Rule1 from './Rule1';
 import Rule2 from './Rule2';
-import NextButton from '../NextButton';
 import LightBulb from './LightBulb';
+import { TypeAnimation } from 'react-type-animation';
 import { useNavigate } from 'react-router-dom';
 
 const BounceButton = () => {
@@ -14,8 +14,9 @@ const BounceButton = () => {
   };
 
   return (
-    <div className="flex justify-center">
-      <div onClick={handleClick} className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
+      <LaterTonight/>
+      <div onClick={handleClick} className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full">
         <svg className="w-6 h-6 text-violet-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
           <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
         </svg>
@@ -23,6 +24,25 @@ const BounceButton = () => {
     </div>
   )
 }
+
+const LaterTonight = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Later tonight...',
+        1000, // wait 1s
+        'In the dormitory...',
+        1000
+      ]}
+      className="text-2xl font-bold mb-4"
+      wrapper="span"
+      speed={50}
+     
+      repeat={Infinity}
+    />
+  );
+};
 
 const Chapter1 = () => {
     return (
