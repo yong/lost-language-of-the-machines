155952cb8338f39d8dd90a8d4152f445bb4d143f
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-30 17:26:42 -0400

    first draft of chat novel

--- src/chapter1/Chat.jsx
+++ src/chapter1/Chat.jsx
@@ -10,7 +10,21 @@ const messages = [
     { d: 1, m: "y" },
     { d: 0, m: "What about the questions that convert Decimal into Binary?" },
     { d: 0, m: "He did not teach us how to do that 😠" },
-    { d: 1, m: "yep, don't know why he left in a hurry" }
+    { d: 1, m: "yep, don't know why he left in a hurry" },
+    { d: 1, m: "but the rule #2 convered in class leaves enough clues" },
+    { d: 0, m: "Huh? How?" },
+    { d: 1, m: "Say the decimal number is 13, we need to break it down into 2's place values - those multiples of two" },
+    { d: 1, m: "13 = 8 + 4 + 1 = 1*8+1*4+0*2+1 = 1101 in binary" },
+    { d: 1, m: "I found it is very tedious to break down larger numbers. Since your calculation unit is way better than my brain on this, may be you can find a better way?" },
+    { d: 0, m: "You are a genius! Let me load this training data..." },
+    { d: 0, m: "I got one! We can figure it out backward, first devide 13 by 2, got 6 and reminder of 1, the reminder will be the rightmost digits of the binary" },
+    { d: 0, m: "keep deviding 6 by 2, got 3 and no reminder, so we have 01" },
+    { d: 1, m: "$#%@#" },
+    { d: 0, m: "Hub? anyway, keep deviding 3 by 2, got 1 and reminder of 1, write it down, 101" },
+    { d: 0, m: "Finally, 1 divided by 2 is 0, reminder is 1, so we have 1101" },
+    { d: 1, m: "RT%$^@#$%@#@##@^#@#$%@#burrrrrrrr" },
+    { d: 0, m: "r u still there? is it your cat typing?" },
+    { d: 0, m: "whatever, I will see you in class tomorrow" },
 ];
 
 const Chat = () => {
--- src/chapter1/Rule2.jsx
+++ src/chapter1/Rule2.jsx
@@ -22,7 +22,8 @@ const Rule2 = () => {
       <div className="text-2xl font-bold">In a number system with base <span className="text-red-600">X</span>, 
       each <span className="text-blue-500">symbol</span>’s place value corresponds to a power of <span className="text-red-600">X</span>, 
       with the exponent being the <span className="text-blue-500">symbol</span>’s <span className="text-green-600">position</span>, starting from <span className="text-green-600">0</span> on the right.</div><br/>
-      For example, <b className="text-blue-500">2583</b> = {DecimalExpression(2,3)} + {DecimalExpression(5,2)} + {DecimalExpression(8,1)} + {DecimalExpression(3, 0)}<br/>
+      For example,<br/>
+      <b className="text-blue-500">2583</b> = {DecimalExpression(2,3)} + {DecimalExpression(5,2)} + {DecimalExpression(8,1)} + {DecimalExpression(3, 0)}<br/>
       <b className="text-blue-500">1101</b> = {BinaryExpression(1, 3)} + {BinaryExpression(1, 2)}+ {BinaryExpression(0, 1)} + {BinaryExpression(1, 0)} = 13 in Decimal
     </div>
   )
