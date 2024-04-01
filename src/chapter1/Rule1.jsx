1082f32edd3674e70c5d856e07b6da180a38f6ea
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-04-01 11:50:39 -0400

    bigger font in the rule file

--- src/chapter1/Rule1.jsx
+++ src/chapter1/Rule1.jsx
@@ -1,14 +1,17 @@
 //Rule1.jsx
 
 const Rule1 = () => {
-    return (<div className="mb-3 text-center bg-blue-200 border-4 border-dashed border-blue-500 p-4 rounded-lg">
-    <div className="text-2xl font-bold">For a <span className="text-red-600">X</span> based number system, there are <span className="text-red-600">X</span> <span className="text-blue-500">symbols</span> to use.</div><br/>
-For example, <span className="text-red-600">10</span> based number system has ten: <b className="text-blue-500">0 1 2 3 4 5 6 7 8 9</b><br/>
-<span className="text-red-600">2</span> based number system has only two: <b className="text-blue-500">1</b> and <b className="text-blue-500">0</b>.<br/>
-<div className="max-w-screen-md mx-auto flex items-center justify-center">
-        <img src="/chapter1/rule1.jpg" alt="rule1" />
+  return (
+      <div className="mb-3 font-mono text-center bg-blue-200 border-4 border-dashed border-blue-500 p-4 rounded-lg">
+          <div className="text-2xl font-bold">For a <span className="text-red-600">X</span> based number system, there are <span className="text-red-600">X</span> <span className="text-blue-500">symbols</span> to use.</div><br/>
+          <div className="text-base">For example,</div>
+          <div className="text-2xl"><span className="text-red-600">10</span> based number system has ten: <b className="text-blue-500">0 1 2 3 4 5 6 7 8 9</b><br/>
+          <span className="text-red-600">2</span> based number system has only two: <b className="text-blue-500">1</b> and <b className="text-blue-500">0</b>.</div><br/>
+          <div className="max-w-screen-md mx-auto flex items-center justify-center">
+              <img src="/chapter1/rule1.jpg" alt="rule1" />
+          </div>
       </div>
-  </div>)
+  )
 }
 
 export default Rule1;
--- src/chapter1/Rule2.jsx
+++ src/chapter1/Rule2.jsx
@@ -22,9 +22,11 @@ const Rule2 = () => {
       <div className="text-2xl font-bold">In a number system with base <span className="text-red-600">X</span>, 
       each <span className="text-blue-500">symbol</span>’s place value corresponds to a power of <span className="text-red-600">X</span>, 
       with the exponent being the <span className="text-blue-500">symbol</span>’s <span className="text-green-600">position</span>, starting from <span className="text-green-600">0</span> on the right.</div><br/>
-      For example,<br/>
-      <b className="text-blue-500">2583</b> = {DecimalExpression(2,3)} + {DecimalExpression(5,2)} + {DecimalExpression(8,1)} + {DecimalExpression(3, 0)}<br/>
-      <b className="text-blue-500">1101</b> = {BinaryExpression(1, 3)} + {BinaryExpression(1, 2)}+ {BinaryExpression(0, 1)} + {BinaryExpression(1, 0)} = 13 in Decimal
+      <div className="text-base">For example,</div>
+      <div className="text-2xl">
+        <b className="text-blue-500">2583</b> = {DecimalExpression(2,3)} + {DecimalExpression(5,2)} + {DecimalExpression(8,1)} + {DecimalExpression(3, 0)}<br/>
+        <b className="text-blue-500">1101</b> = {BinaryExpression(1, 3)} + {BinaryExpression(1, 2)}+ {BinaryExpression(0, 1)} + {BinaryExpression(1, 0)} = 13 in Decimal
+      </div>
       <div className="max-w-screen-md mx-auto flex items-center justify-center">
         <img src="/chapter1/rule2.jpg" alt="rule2" />
       </div>
