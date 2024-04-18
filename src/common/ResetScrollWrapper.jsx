3c46ec6e651b748274774d902df6cc7446147e26
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-04-17 23:37:55 -0400

    try window.scrollTo

--- src/common/ResetScrollWrapper.jsx
+++ src/common/ResetScrollWrapper.jsx
@@ -5,7 +5,7 @@ import { useLayoutEffect } from 'react';
 const ResetScrollWrapper = ({children}) => {
     const location = useLocation();
     useLayoutEffect(() => {
-      document.documentElement.scrollTo(0, 0);
+      window.scrollTo(0, 0);
     }, [location.pathname]);
     return children
   }
