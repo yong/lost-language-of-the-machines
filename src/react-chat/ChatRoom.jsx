e6336f0f2bc8f47a641b2b104a609874cb83c519
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-05-21 16:07:24 -0400

    rewrite chat with tailwind

--- src/react-chat/ChatRoom.jsx
+++ src/react-chat/ChatRoom.jsx
@@ -1,31 +1,25 @@
 import React, { useState } from 'react';
-//import './ChatRoom.css';
 
 const MessageItem = ({ d, m, s }) => {
-	const messagePosition = d === 1
-		? 'chatApp__convMessageItem--right'
-		: 'chatApp__convMessageItem--left';
-
-	const senderAvatar = d === 1
-		? '/starlaxverse_avatar.png'
-		: '/flamey_avatar.png';
+	const messagePosition = d === 1 ? 'flex-row-reverse' : 'flex-row';
+	const senderAvatar = d === 1 ? '/starlaxverse_avatar.png' : '/flamey_avatar.png';
 
 	return (
-		<div className={`chatApp__convMessageItem ${messagePosition} clearfix`}>
-			<img src={senderAvatar} alt="Avatar" className="chatApp__convMessageAvatar" />
-			<div className="chatApp__convMessageValue">
-			{s ? (
-				<pre>
-				{m.split('\n').map((line, i) => (
-					<React.Fragment key={i}>
-					{line}
-					<br/>
-					</React.Fragment>
-				))}
-				</pre>
-			) : (
-				m
-			)}
+		<div className={`flex ${messagePosition} mb-4`}>
+			<img src={senderAvatar} alt="Avatar" className="w-10 h-10 rounded-full" />
+			<div className={`ml-4 mr-4 p-2 rounded-lg ${d === 1 ? 'bg-gray-200' : 'bg-blue-500 text-white'}`}>
+				{s ? (
+					<pre>
+						{m.split('\n').map((line, i) => (
+							<React.Fragment key={i}>
+								{line}
+								<br/>
+							</React.Fragment>
+						))}
+					</pre>
+				) : (
+					m
+				)}
 			</div>
 		</div>
 	);
@@ -33,7 +27,7 @@ const MessageItem = ({ d, m, s }) => {
 
 const MessageList = ({ messages }) => {
 	return (
-		<div className="max-w-screen-md mx-auto bg-yellow-300">
+		<div className="max-w-md mx-auto bg-yellow-300 p-4">
 			{messages.map((messageItem, index) => (
 				<MessageItem
 					key={index}
@@ -50,7 +44,7 @@ const ChatRoom = ({initialMessages}) => {
 	const [messages, setMessages] = useState(initialMessages);
 
 	return (
-		<div className="chatApp__room">
+		<div className="p-4">
 			<MessageList messages={messages} />
 		</div>
 	);
