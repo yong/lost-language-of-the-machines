534db7b9933014c65966907e5f262fd030201745
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-05-31 16:56:20 -0400

    change title of coming soon page

--- src/pages/chapter4/index.tsx
+++ src/pages/chapter4/index.tsx
@@ -1,14 +1,16 @@
 import { NextPage } from 'next';
-
-import withCover from '../../components/common/withCover';
+import Head from 'next/head';
 
 import ClockFont from '@/components/demo/ClockFont';
 
 const App: NextPage = () => {
   return (
     <div className='text-center p-12'>
-    <ClockFont chars="coming soon"/><br/>    
-    <p>Watch our <a className="text-blue-500 underline hover:text-blue-800" href="http://github.com/yong/LostLanguageOfTheMachines">github page</a> for updates</p>
+      <Head>
+        <title>Coming Soon - Lost Language of the Machines</title>
+      </Head>
+      <ClockFont chars="coming soon"/><br/>
+      <p>Watch our <a className="text-blue-500 underline hover:text-blue-800" href="http://github.com/yong/LostLanguageOfTheMachines">github page</a> for updates</p>
     </div>
   );
 }
