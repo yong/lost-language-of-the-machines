6f9e932e0eeff2061cd8bf1174a68c6928bf9d1a
Yong Zhi <xue.yong.zhi@gmail.com>
2026-04-04 13:56:01 -0400

    test deploy: add exclamation to coming soon

--- src/pages/chapter4/index.tsx
+++ src/pages/chapter4/index.tsx
@@ -9,7 +9,7 @@ const App: NextPage = () => {
       <Head>
         <title>Coming Soon - Lost Language of the Machines</title>
       </Head>
-      <ClockFont chars="coming soon"/><br/>
+      <ClockFont chars="coming soon!"/><br/>
       <p>Watch our <a className="text-blue-500 underline hover:text-blue-800" href="http://github.com/yong/LostLanguageOfTheMachines">github page</a> for updates</p>
     </div>
   );
